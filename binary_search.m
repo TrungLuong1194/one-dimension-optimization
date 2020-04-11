1463aab7b79f172a69e3d7a1a1c99f56834a69e5
TrungLuong1194 <trungluong1194@gmail.com>
2020-04-11 14:52:53 +0300

    update

--- binary_search.m
+++ binary_search.m
@@ -3,59 +3,74 @@ function binary_search()
   
   a = -1;
   b = 0;
-  list_x_star = [];
-  list_f_star = [];
-  % Output x_star, f_star
-  for epsilon = [10.^-2, 10.^-4, 10.^-6]
-    [x_star, f_star, countIter] = binary(a, b, epsilon);
-    list_x_star = [list_x_star, x_star];
-    list_f_star = [list_f_star, f_star];
-    fprintf('epsilon: %f\n', epsilon);
-    fprintf('x_star: %f\n', x_star);
-    fprintf('f_star: %f\n', f_star);
-    fprintf('Number of iteration: %d\n', countIter);
-    fprintf('-------------------------------------\n');
-  end
+  epsilon = power(10, -6);
   
   % Draw graph function
-  x = [-1 : 0.01 : -0.02];
+  x = [-1 : 0.01 : 0];
   y = [];
   for i = x
     y = [y, func(i)];
   end
-  scatter(x, y, 'filled'), xlabel('x'), ylabel('func(x)'), title('Graph')
+  plot(x, y), xlabel('x'), ylabel('func(x)'), title('Graph')
   
-  hold on
+  hold on;
+  
+  [x_star, f_star, countIter] = binary(a, b, epsilon);
+  fprintf('-------------------------------------\n');
+  fprintf('epsilon: %f\n', epsilon);
+  fprintf('x_star: %f\n', x_star);
+  fprintf('f_star: %f\n', f_star);
+  fprintf('Number of iteration: %d\n', countIter);
+  
+  plot(x_star, f_star, 'Color', 'g', 'MarkerSize', 25);
 
-  scatter(list_x_star, list_f_star, 'r', 'filled'), legend('(x,f(x))', '(x*,f(x*))')
-
-  hold off
+  hold off;
 end
 
 % Input function
-function value = func(x)
-  value = log(2*(x.^5) - 7*x + sqrt(11)) + ...
-  sinh((-4*(x.^2) - 4*x + 3 - 4*sqrt(2)) / (3*(x.^2) + 3*x + 3*sqrt(2))) - 1.0;
+function y = func(x)
+  y1 = 2 * power(x, 5) - 7 * x + sqrt(11);
+  y2 = (-4*x*x - 4*x + 3 - 4*sqrt(2)) / (3*x*x + 3*x + 3*sqrt(2));
+  y = log(y1) + sinh(y2) - 1.0;
 end
 
 % Binary search function
 function [x_star, f_star, countIter] = binary(a, b, epsilon)
-  x = (a+b) / 2;
-  alpha = (b-a) / 2;
-  countIter = 0;
+  delta = (b-a)/2;
+  x0 = a;
+  f0 = func(x0);
+  countIter = 1;
+  flag = true;
   
-  while alpha >= epsilon
+  fprintf('N = %d\tdelta = %.9f\t(x0, f0) = (%.9f, %.9f)\n', countIter, delta, x0, f0);
+  plot(x0, f0, 'Marker', '.', 'Color', 'r', 'MarkerSize', 25);
+  
+  while flag
     countIter = countIter + 1;
-    if func(x + epsilon) > func(x)
-      p = -1;
-    else
-      p = 1;
-    end
+    x1 = x0 + delta;
+    f1 = func(x1);
     
-    alpha = alpha / 2;
-    x = x + alpha*p;
+    fprintf('N = %d\tdelta = %.9f\t(x0, f0) = (%.9f, %.9f)\t(x1, f1) = (%.9f, %.9f)\n', countIter, delta, x0, f0, x1, f1);
+    plot(x0, f0, 'Marker', '.', 'Color', 'r', 'MarkerSize', 25);
+    
+    if f0 > f1
+      x0 = x1;
+      f0 = f1;
+      
+      if a < x0 && x0 < b
+        continue;
+      end
+    end
+      
+    if abs(delta) <= epsilon
+      flag = false;
+    else
+      x0 = x1;
+      f0 = f1;
+      delta = -delta/2;
+    end
   end
   
-  x_star = x;
-  f_star = func(x_star);
+  x_star = x0;
+  f_star = f0;
 end
--- golden_section_search.m
+++ golden_section_search.m
@@ -3,61 +3,69 @@ function golden_section_search()
   
   a = -1;
   b = 0;
-  list_x_star = [];
-  list_f_star = [];
-  % Output x_star, f_star
-  for epsilon = [10.^-2, 10.^-4, 10.^-6]
-    [x_star, f_star, countIter] = golden(a, b, epsilon);
-    list_x_star = [list_x_star, x_star];
-    list_f_star = [list_f_star, f_star];
-    fprintf('epsilon: %f\n', epsilon);
-    fprintf('x_star: %f\n', x_star);
-    fprintf('f_star: %f\n', f_star);
-    fprintf('Number of iteration: %d\n', countIter);
-    fprintf('-------------------------------------\n');
-  end
+  epsilon = power(10, -6);
   
   % Draw graph function
-  x = [-1 : 0.01 : -0.02];
+  x = [-1 : 0.01 : 0];
   y = [];
   for i = x
     y = [y, func(i)];
   end
-  scatter(x, y, 'filled'), xlabel('x'), ylabel('func(x)'), title('Graph')
+  plot(x, y), xlabel('x'), ylabel('func(x)'), title('Graph')
   
-  hold on
+  hold on;
+  
+  [x_star, f_star, countIter] = golden(a, b, epsilon);
+  fprintf('-------------------------------------\n');
+  fprintf('epsilon: %f\n', epsilon);
+  fprintf('x_star: %f\n', x_star);
+  fprintf('f_star: %f\n', f_star);
+  fprintf('Number of iteration: %d\n', countIter);
+  
+  plot(x_star, f_star, 'Color', 'g', 'MarkerSize', 25);
 
-  scatter(list_x_star, list_f_star, 'r', 'filled'), legend('(x,f(x))', '(x*,f(x*))')
-
-  hold off
+  hold off;
 end
 
 % Input function
-function value = func(x)
-  value = log(2*(x.^5) - 7*x + sqrt(11)) + ...
-  sinh((-4*(x.^2) - 4*x + 3 - 4*sqrt(2)) / (3*(x.^2) + 3*x + 3*sqrt(2))) - 1.0;
+function y = func(x)
+  y1 = 2 * power(x, 5) - 7 * x + sqrt(11);
+  y2 = (-4*x*x - 4*x + 3 - 4*sqrt(2)) / (3*x*x + 3*x + 3*sqrt(2));
+  y = log(y1) + sinh(y2) - 1.0;
 end
 
 % Binary search function
 function [x_star, f_star, countIter] = golden(a, b, epsilon)
-  countIter = 0;
-  gr = (sqrt(5) - 1) / 2;
-  x1 = b - gr*(b-a);
-  x2 = a + gr*(b-a);
+  countIter = 1;
+  t = (sqrt(5) - 1) / 2;
+  l = b - a;
+  x1 = b - t*l;
+  f1 = func(x1);
+  x2 = a + t*l;
+  f2 = func(x2);
   
-  while abs(b - a) >= epsilon
+  fprintf('N = %d\tl = %d\t(x1, f1) = (%.9f, %.9f)\t(x2, f2) = (%.9f, %.9f)\n', countIter, l, x1, f1, x2, f2);
+  
+  while l > 2*epsilon
     countIter = countIter + 1;
-    if func(x1) < func(x2)
-      a = a;
+    if f1 <= f2
       b = x2;
+      l = b - a;
       x2 = x1;
-      x1 = b - gr*(b-a);
+      f2 = f1;
+      x1 = b - t*l;
+      f1 = func(x1);
     else
       a = x1;
-      b = b;
+      l = b - a;
       x1 = x2;
-      x2 = a + gr*(b-a);
+      f1 = f2;
+      x2 = a + t*l;
+      f2 = func(x2);
     end
+    
+    fprintf('N = %d\tl = %d\t(x1, f1) = (%.9f, %.9f)\t(x2, f2) = (%.9f, %.9f)\n', countIter, l, x1, f1, x2, f2);
+    plot(x1, f1, 'Marker', '.', 'Color', 'r', 'MarkerSize', 25);
   end
   
   x_star = (a + b) / 2;
